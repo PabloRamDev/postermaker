205a18ade469cab4744fa62a593d45613d30c21f
pabloram.dev <pabloram.dev@gmail.com>
2025-10-10 16:03:42 -0300

    add theme provider frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
-import { Dropzone } from "./components/ui/shadcn-io/dropzone"
-
 function App() {
 
   return (
-    <>
-      <main className="dark flex flex-col h-screen w-screen min-h-sreen min-w-screen bg-default-background text-default-font font-caption">
-        <h1 className="text-3xl">
+
+      <main className="container flex flex-col h-screen w-screen min-h-sreen min-w-screen justify-center items-center">
+        <h1 className="text-3xl font-bold">
         POSTER MAKER FRONT
         </h1>
-        <Dropzone />
         <p>hola</p>
       </main>
-    </>
   )
 }
 
